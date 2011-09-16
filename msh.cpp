5e223946b35bbc4eff64ec81b198fa254c2caf87
Katons <guy@kat.(none)>
2011-09-17 01:41:19 +0300

    C++ Math - starting with integers, and a shell

---
+++ msh.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+#include "bits.hpp"
+using namespace BitMath;
+
+int main()
+{
+	typedef map<std::string, Int> symbols;
+	while (true) {
+		string line;
+		getline(cin, line);
+		istringstream iss(line);
+		
+		string arr[10];
+		
+		while (!iss.eof()) {
+			string str;
+			iss >> str;
+			
+	}
+	return 0;
+}
